f3b9bf364834b9a9ecda71f11dde1f88b029767c
somramnani <som.ramnani@gmail.com>
2021-05-02 14:10:49 -0400

    adds Prices.jsx component

---
+++ components/Prices.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+class Prices extends React.Component {
+  state = {
+    currency: 'USD'
+  }
+  render(){
+    return(
+      <div>
+        Prices
+      </div>
+    )
+  }
+}
+export default Prices;
