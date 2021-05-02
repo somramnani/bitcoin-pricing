940064bb34ad20dbb92302cec0b68737330cf929
somramnani <som.ramnani@gmail.com>
2021-05-02 12:47:54 -0400

    Edits index.js & about.js: adds routes

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,17 @@
-export default()=> <div>Hell World</div>
+import Link from 'next/link';
+
+const Index = () =>(
+  <div>
+    
+    <ul>
+      <li><Link href ="/"><a>Home</a></Link></li>
+      <li><Link href ="/about"><a>About</a></Link></li>
+    </ul>
+      
+    <h1>
+      Welcome to BitzPrice
+    </h1>
+    </div>
+)
+
+export default Index;
